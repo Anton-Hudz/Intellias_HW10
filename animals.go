6b688e50d157f3c3587493efcc4efe4a42ce8821
Anton Hudz <pat280885@mail.ru>
2022-07-07 12:16:44 +0300

    add code

--- animals.go
+++ animals.go
@@ -5,3 +5,72 @@ const (
 	feedingNormCat = 7
 	feedingNormCow = 25
 )
+
+type dog struct {
+	typeOfAnimal string
+	nameOfAnimal string
+	animalWeight int
+	feedingNorm  int
+}
+
+func (d dog) gettypeOfAnimal() string {
+	return d.typeOfAnimal
+}
+
+func (d dog) getnameOfAnimal() string {
+	return d.nameOfAnimal
+}
+
+func (d dog) getanimalWeight() int {
+	return d.animalWeight
+}
+
+func (d dog) getFeedingNorm() int {
+	return d.feedingNorm
+}
+
+type cat struct {
+	typeOfAnimal string
+	nameOfAnimal string
+	animalWeight int
+	feedingNorm  int
+}
+
+func (c cat) gettypeOfAnimal() string {
+	return c.typeOfAnimal
+}
+
+func (c cat) getnameOfAnimal() string {
+	return c.nameOfAnimal
+}
+
+func (c cat) getanimalWeight() int {
+	return c.animalWeight
+}
+
+func (c cat) getFeedingNorm() int {
+	return c.feedingNorm
+}
+
+type cow struct {
+	typeOfAnimal string
+	nameOfAnimal string
+	animalWeight int
+	feedingNorm  int
+}
+
+func (c cow) gettypeOfAnimal() string {
+	return c.typeOfAnimal
+}
+
+func (c cow) getnameOfAnimal() string {
+	return c.nameOfAnimal
+}
+
+func (c cow) getanimalWeight() int {
+	return c.animalWeight
+}
+
+func (c cow) getFeedingNorm() int {
+	return c.feedingNorm
+}
--- main.go
+++ main.go
@@ -1,5 +1,74 @@
 package main
 
+import "fmt"
+
+type Animals interface {
+	typeForAnimal
+	nameForAnimal
+	weightForAnimal
+	foodForAnimal
+}
+
+type typeForAnimal interface {
+	gettypeOfAnimal() string
+}
+
+type nameForAnimal interface {
+	getnameOfAnimal() string
+}
+type weightForAnimal interface {
+	getanimalWeight() int
+}
+
+type foodForAnimal interface {
+	getFeedingNorm() int
+}
+
+func main() {
+	allAnimalsInFarm := []Animals{
+		dog{
+			typeOfAnimal: "Dog",
+			nameOfAnimal: "Dog11",
+			animalWeight: 15,
+			feedingNorm:  feedingNormDog,
+		},
+		cat{
+			typeOfAnimal: "Cat",
+			nameOfAnimal: "Cat11",
+			animalWeight: 5,
+			feedingNorm:  feedingNormCat,
+		},
+		cow{
+			typeOfAnimal: "Cow",
+			nameOfAnimal: "Cow11",
+			animalWeight: 300,
+			feedingNorm:  feedingNormCow,
+		},
+		cow{
+			typeOfAnimal: "Cow",
+			nameOfAnimal: "Cow22",
+			animalWeight: 250,
+			feedingNorm:  feedingNormCow,
+		},
+	}
+
+	var resultAllFoodForAllAnimals int
+
+	for _, v := range allAnimalsInFarm {
+		needFoodPerWeight := v.getanimalWeight() * v.getFeedingNorm()
+		resultAllFoodForAllAnimals += needFoodPerWeight
+		fmt.Printf(
+			"Animal type: %s, name: %s, weight: %d kg, need feed per month: %d kg\n",
+			v.gettypeOfAnimal(),
+			v.getnameOfAnimal(),
+			v.getanimalWeight(),
+			needFoodPerWeight,
+		)
+	}
+	fmt.Printf("All animals need %d kg of feed per month", resultAllFoodForAllAnimals)
+
+}
+
 // Домашнє завдання по інтерфейсах:
 // кожна тварина в залежності від свого типу споживає різку кількість
 // кілограмів їжі на місяць
